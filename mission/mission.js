891674318da05df9162c593c1bf6e4202d6f34e2
sorens-it <sorens.it@gmail.com>
2024-09-28 22:11:43 -0700

    wk2
    Signed-off-by: sorens-it <sorens.it@gmail.com>

--- mission/mission.js
+++ mission/mission.js
@@ -1,8 +1,14 @@
 function setTheme() {
-    // Set the light theme by default
-    document.body.classList.remove("dark");
-    document.querySelector("img").setAttribute("src", "./byui-logo_blue.webp");
+    const themeValue = document.querySelector("select").value;
+    console.log("Theme" + themeValue);
+    
+    if (themeValue === "Dark"){
+        document.body.classList.add("dark");
+        document.querySelector("img").setAttribute("src", "./byui-logo_white.png");
+    }
+    else {
+        document.body.classList.remove("dark");
+        document.querySelector("img").setAttribute("src", "./byui-logo_blue.webp");
+    }
 }
-
-// Ensure the theme is set when the page loads
-setTheme();
+document.querySelector("select").addEventListener("change", setTheme);
