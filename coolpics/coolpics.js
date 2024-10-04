b98c74f270b1e3c0cb1bc57b330ec1c944cbb810
sorens-it <sorens.it@gmail.com>
2024-10-04 09:43:25 -0700

    w3coolpics
    Signed-off-by: sorens-it <sorens.it@gmail.com>

---
+++ coolpics/coolpics.js
@@ -0,0 +1,8 @@
+document.getElementById("menu").addEventListener("click", function () {
+    const navLinks = document.getElementById("nav-links");
+    if (navLinks.style.display === "block") {
+        navLinks.style.display = "none";
+    } else {
+        navLinks.style.display = "block";
+    }
+});
